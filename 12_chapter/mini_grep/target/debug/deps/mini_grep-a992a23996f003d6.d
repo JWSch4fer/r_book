9381c945092b45d64909aa3b41109b4c798af6c7
JWSch4fer <joeschafer28@gmail.com>
2025-07-02 10:34:09 -0500

    start cli tool

---
+++ 12_chapter/mini_grep/target/debug/deps/mini_grep-a992a23996f003d6.d
@@ -0,0 +1,5 @@
+C:\Users\joesc\RUST\r_book\12_chapter\mini_grep\target\debug\deps\libmini_grep-a992a23996f003d6.rmeta: src\main.rs
+
+C:\Users\joesc\RUST\r_book\12_chapter\mini_grep\target\debug\deps\mini_grep-a992a23996f003d6.d: src\main.rs
+
+src\main.rs:
---
+++ 12_chapter/mini_grep/target/debug/mini_grep.d
@@ -0,0 +1,1 @@
+C:\Users\joesc\RUST\r_book\12_chapter\mini_grep\target\debug\mini_grep.exe: C:\Users\joesc\RUST\r_book\12_chapter\mini_grep\src\main.rs
